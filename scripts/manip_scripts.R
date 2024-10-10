bebdcae45eb4ae732837c078e946ca520a5eaa63
unknown <nzimbabeni@yahoo.com>
2024-10-10 15:00:33 +0100

    première instruction à manipuler avec Rstudio

---
+++ scripts/manip_scripts.R
@@ -0,0 +1,155 @@
+### packages à télécharger
+
+library(tidyverse)
+library(dplyr)
+library(nycflights13)
+
+### data d'expérimentations
+
+data("flights")
+data("airports")
+data("airlines")
+
+### renomer les datasets
+
+df.1= flights
+df.2= airports
+df.3= airlines
+
+### structures des datasets
+
+str(df.1)
+class(df.1)
+names(df.1)
+View(df.1)
+
+### commande slice 
+
+slice(df.1, 10)
+slice(df.1, 1:6)   ## slice peut aussi afficher le même résultat que head(df.1)
+head(df.1)
+
+### commande filter
+
+unique(df.1$month)
+filter(df.1, month==1)
+filter(df.1, dep_time>500| dep_time<1000)
+filter(df.1, dep_time>500 & dep_time<1000)%>%
+  #select(df.1, det_time)
+    #summary(df.1$dep_time)
+
+filter(df.1, distance==max(distance))
+
+### commande select
+
+names(df.2)
+select(df.2, lat, lon)
+dim(df.2)
+
+### les compléments dans les commandes select, starts_with, ends_with, contains ou matches
+
+select(df.1, starts_with("dep_"))
+select(df.1, ends_with("e"))
+select(df.1, contains("ou"))
+
+###all_of et any_of permettent de fournir une liste de variables 
+#à extraire sous forme de vecteur textuel. Alors que all_of renverra une erreur 
+#si une variable n’est pas trouvée dans le tableau de départ, any_of sera moins stricte.
+
+select(df.1, all_of(c("year", "month", "day","beni")))
+select(df.1, any_of(c("year", "month", "day","beni")))
+
+##where permets de sélectionner des variables à partir d’une fonction qui renvoie une valeur logique. 
+#Par exemple, pour sélectionner seulement les variables textuelles.
+str(df.1)
+select(df.1, where(is.character))
+select(df.1, where(is.numeric))
+
+##select peut être utilisée pour réordonner les colonnes d’une table 
+#en utilisant la fonction everything(), qui sélectionne l’ensemble des colonnes non encore sélectionnées. 
+#Ainsi, si on souhaite faire passer la colonne name en première position de la table airports, on peut faire :
+
+select(df.2, name, lat, lon,everything())
+
+#Pour réordonner des colonnes, on pourra aussi avoir recours à relocate en indiquant les premières variables. 
+#IL n’est pas nécessaire d’ajouter everything() car avec relocate toutes les variables sont conservées.
+
+relocate(df.2, lon, lat, name)
+
+#Une variante de select est rename, qui permet de renommer facilement des colonnes. 
+#On l’utilise en lui passant des paramètres de la forme nouveau_nom = ancien_nom. 
+#Ainsi, si on veut renommer les colonnes lon et lat de airports en longitude et latitude :
+
+dplyr::rename(df.2, longitude=lon, latitude=lat, altitude=alt)
+
+##arrange
+#arrange réordonne les lignes d’un tableau selon une ou plusieurs colonnes.
+#Ainsi, si on veut trier le tableau flights selon le retard au départ croissant :
+
+dplyr::arrange(df.1, month)
+dplyr::arrange(df.1, dep_delay)
+
+dplyr::arrange(flights, month, dep_delay)
+
+#Si on veut trier selon une colonne par ordre décroissant, on lui applique la fonction desc() : 
+# ordre decroissant
+
+dplyr::arrange(df.1, desc(dep_delay))
+
+##Combiné avec slice, arrange permet 
+#par exemple de sélectionner les trois vols ayant eu le plus de retard :
+
+tmp = dplyr::arrange(df.1, desc(dep_delay))
+slice(tmp,1:3)
+
+#mutate
+#mutate permet de créer de nouvelles colonnes dans le tableau de données, 
+#en général à partir de variables existantes.
+
+#Par exemple, la table airports contient l’altitude de l’aéroport en pieds. 
+#Si on veut créer une nouvelle variable alt_m avec l’altitude en mètres, on peut faire :
+
+df.2 <- mutate(df.2, alt_m = alt / 3.2808)
+select(df.2, name, alt, alt_m)
+
+#On peut créer plusieurs nouvelles colonnes en une seule fois, 
+#et les expressions successives peuvent prendre en compte les résultats des calculs précédents. 
+#L’exemple suivant convertit d’abord la distance en kilomètres dans une variable distance_km, 
+#puis utilise cette nouvelle colonne pour calculer la vitesse en km/h.
+
+df.1 <- mutate(df.1, 
+                  distance_km = distance / 0.62137,
+                  vitesse = distance_km / air_time * 60)
+select(df.1, distance, distance_km, vitesse)
+
+#À noter que mutate est évidemment parfaitement compatible avec les fonctions vues dans le chapitre sur les recodages : 
+#fonctions de forcats, if_else, case_when…
+#L’avantage d’utiliser mutate est double. 
+#D’abord il permet d’éviter d’avoir à saisir le nom du tableau de données dans les conditions d’un if_else ou d’un case_when :
+
+df.1 <- mutate(df.1,
+                  type_retard = case_when(
+                    dep_delay > 0 & arr_delay > 0 ~ "Retard départ et arrivée",
+                    dep_delay > 0 & arr_delay <= 0 ~ "Retard départ",
+                    dep_delay <= 0 & arr_delay > 0 ~ "Retard arrivée",
+                    TRUE ~ "Aucun retard"))
+
+#Utiliser mutate pour les recodages permet aussi de les intégrer dans un pipeline de traitement de données, 
+#concept présenté dans la section suivante.
+#Citons également les fonctions "recode" et "recode_factor".
+
+df.1$month_name <- recode_factor(flights$month,
+                                    "1" = "Jan",
+                                    "2" = "Feb",
+                                    "3" = "Mar",
+                                    "4" = "Apr",
+                                    "5" = "May",
+                                    "6" = "Jun",
+                                    "7" = "Jul",
+                                    "8" = "Aug",
+                                    "9" = "Sep",
+                                    "10" = "Oct",
+                                    "11" = "Nov",
+                                    "12" = "Dec"
+)
+
